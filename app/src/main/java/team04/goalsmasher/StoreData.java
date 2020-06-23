2306e31d81cf796ed624336ab2627fd8309f700f
Spencer Beaumont <spence.beaumont@gmail.com>
2020-06-23 16:35:24 -0600

    fixed import errors with the new file structure and added/changed the comments

--- app/src/main/java/team04/goalsmasher/StoreData.java
+++ app/src/main/java/team04/goalsmasher/StoreData.java
@@ -4,14 +4,19 @@ import com.google.gson.Gson;
 
 import java.util.ArrayList;
 
+import team04.goalsmasher.model.GoalSmasherModel;
+
 public class StoreData {
     private Gson gson = new Gson();
 
+    /* saveData: This method converts the users data to a json
+     * string and saves the data to internal storage */
     public void saveData(ArrayList<GoalSmasherModel> goalList) {
         String goalJson = gson.toJson(goalList);
     }
 
-    //this is assuming we are storing a list of goals. practice git.
+    /* loadData: Loads the users data from internal storage
+     * this is assuming we are storing a list of goals */
     public ArrayList<GoalSmasherModel> loadData() {
         ArrayList<GoalSmasherModel> goalList = new ArrayList<>();
         return goalList;

cb0a37f0700016fcb1bc0e80e5158e81ff2f796b
nnemeth21 <nem18002@byui.edu>
2020-06-22 18:47:51 -0600

    changed class name as a test in my branch setup

--- app/src/main/java/team04/goalsmasher/GoalCreate.java
+++ app/src/main/java/team04/goalsmasher/GoalCreate.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class createGoalActivity extends AppCompatActivity {
+public class GoalCreate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

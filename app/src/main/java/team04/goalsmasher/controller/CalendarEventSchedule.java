d343c40110514a3ab7d7df1cbae513c6e6dc697d
Colwelljj <col18046@byui.edu>
2020-06-24 13:50:32 -0600

    added code to the calendarEvents

--- app/src/main/java/team04/goalsmasher/controller/CalendarEventSchedule.java
+++ app/src/main/java/team04/goalsmasher/controller/CalendarEventSchedule.java
@@ -1,18 +1,35 @@
 package team04.goalsmasher.controller;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import team04.goalsmasher.R;
 
 // This is the Calendar code for the app
 
 public class CalendarEventSchedule extends AppCompatActivity {
+    CalendarView calendarView;
+    TextView calendarGoals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar_event_schedule);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarGoals = (TextView) findViewById(R.id.calendarGoals);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (year) + "/" + month + 1 + "/" + dayOfMonth;
+                calendarGoals.setText(date);
+            }
+        });
+
     }
 }
